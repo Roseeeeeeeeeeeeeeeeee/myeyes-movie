9b39f6453f1ae5173eed39c0fe780b8ce74f5fbb
Roseeeeeeeeeeeeeeeeee <165487175+Roseeeeeeeeeeeeeeeeee@users.noreply.github.com>
2025-06-18 20:59:33 +0800

    定义数据库模型

---
+++ server/src/db/movieSchema.ts
@@ -0,0 +1,18 @@
+import Mongoose from 'mongoose'
+import { Movie } from '../entities/movie'
+interface IMovie  extends Movie,Mongoose.Document{}
+ const movieSchema = new Mongoose.Schema<IMovie>({
+    name: String,
+    types: [String],
+    areas: [String],
+    timeDuration: Number,
+    isHot: Boolean,
+    isComing: Boolean,
+    isClassic: Boolean,
+    description:String,
+    poster:String
+
+ },{
+    versionKey:false
+ })
+ export default Mongoose.model<IMovie>('Movie',movieSchema)
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,4 @@
-import { validate } from "class-validator";
-import { Movie } from "./entities/movie";
-import { plainToInstance } from "class-transformer";
-
-const mov:any= {}
-mov.name = 111;
-mov.types = ['政治'];
-mov.timeDuration = 1.2;
-const movie = plainToInstance(Movie,mov);
-console.log(movie);
-
-validate(movie).then(err=>{
-    console.log(err)
+import { MovieModel } from "./db";
+MovieModel.find().then(movs=>{
+    console.log(movs);
 })
